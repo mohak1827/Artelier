e1ad19a8cd9123c223d94df08376d6f52d3a21f5
mohak1827 <mohak1827vk@gmail.com>
2025-11-26 04:49:23 +0530

    Configure Vite API URL

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default api;
